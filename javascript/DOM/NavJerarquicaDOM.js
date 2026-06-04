0b3b2ad4fd63b5a18e7c26ba85d8578475d13c21
JulianChavezDev <julicgavez1@gmail.com>
2026-06-04 13:55:25 +0200

    adding DOM documentaries

---
+++ javascript/DOM/NavJerarquicaDOM.js
@@ -0,0 +1,52 @@
+//Buscar al padre, en este caso una etiqueta UL
+const parent = document.getElementById("parent");
+console.log("parent");
+
+//Buscar a los hijos de UL, los LI
+const children = parent.children;
+console.log(children);
+
+//Trae un objeto con todos los nodos de los HIJOS del elemento PADRE, incluidos los saltos de línea, a los que se les denomina #text
+const childNodes = parent.childNodes;
+console.log(childNodes);
+
+//Sirve para traer el primer nodo del primer HIJO LI
+const children2 = parent.firstChild;
+console.log(children2);
+
+//Llegar al PRIMER HIJO
+const firstChild = parent.firstElementChild;
+console.log(firstChild);
+
+//Trae al ÚLTIMO nodo de los HIJOS del elemento PADRE
+const lastChild2 = parent.lastChild;
+console.log(lastChild2);
+
+//Legar al ÚLTIMO HIJO
+const lastChild = parent.lastElementChild;
+console.log(lastChild);
+```También les dejo los métodos para **navegar entre elementos hermanos:**
+
+
+
+```js
+//Trae al nodo anterior del elemento HERMANO del padre
+const previousSibling2 = parent.previousSibling;
+console.log(previousSibling2);
+
+//Legar al elemento ANTES del padre, es decir,su hermano
+const previousSibling = parent.previousElementSibling;
+console.log(previousSibling);
+
+//Llegar al nodo del elemento HERMANO, es decir, DESPUÉS del padre
+const nextSibling2 = parent.nextSibling;
+console.log(nextSibling2);
+
+//Llegar al elemento DESPUÉS del padre, es decir, también su hermano pero después de este
+const nextSibling = parent.nextElementSibling;
+console.log(nextSibling);
+```Acá se incluyen los métodos que el profe nos colocó en comentarios para investigar por cuenta propia 😃
+
+
+
+Mi perfil de Github: <https://github.com/iamanyyeei>
---
+++ javascript/DOM/SeleccionElementos.js
@@ -0,0 +1,36 @@
+/*
+
+Para obtener elementos del DOM en JavaScript, puedes usar las siguientes formas:
+
+Por ID: document.getElementById('miId') - Devuelve el elemento único con el ID especificado.
+
+Por clase: document.getElementsByClassName('miClase') - Devuelve una colección de todos los elementos que tienen la clase especificada.
+
+Por etiqueta: document.getElementsByTagName('miEtiqueta') - Devuelve una colección de elementos que coinciden con la etiqueta indicada.
+
+Selector CSS: document.querySelector('selectorCSS') - Devuelve el primer elemento que coincide con el selector CSS.
+
+Selector CSS para múltiples elementos: document.querySelectorAll('selectorCSS') - Devuelve todos los elementos que coinciden con el selector CSS.
+
+Estas son las maneras más comunes de seleccionar elementos, facilitando la manipulación del DOM.e");
+ta");
+
+
+
+
+
+ */
+
+/* EJEMPLOS */
+
+document.getElementById("app-title")
+
+document.querySelector("#app-title");
+
+document.querySelector("p");
+
+document.getElementsByClassName("menu-items");
+
+document.getElementsByTagName("p");
+
+document.querySelectorAll(".menu-items");
